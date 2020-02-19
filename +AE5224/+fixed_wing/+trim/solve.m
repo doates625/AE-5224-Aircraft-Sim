d3e202320876b02d93b4d941f0a3a2b9e0afe385
Dan <doates625@gmail.com>
2020-02-18 20:36:18 -0500

    Simplified fixed-wing numerical trim solver

--- +AE5224/+fixed_wing/+trim/solve.m
+++ +AE5224/+fixed_wing/+trim/solve.m
@@ -11,7 +11,7 @@ function [x_st, u_st] = solve(model, trim)
 %   - u_st = Trim controls [d_e; d_a; d_r; d_p]
 
 % Imports
-import('AE5224.rigid_body.Model.pack_x');
+import('AE5224.rigid_body.Model');
 import('quat.Quat');
 
 % Symbolic unknowns
@@ -20,20 +20,19 @@ w_b = sym('w_b', [3, 1]);
 u_st = sym('u', [4, 1]);
 va_b = zeros(3, 1);
 
-% Symbolic forces and moments
+% Symbolic dynamics
 R_eb = Quat(q_e).conj().mat_rot();
-x_st = model.pack_x(trim.p_e, q_e, trim.v_e, w_b);
-[F_b, M_b] = model.forces(x_st, [u_st; va_b]);
+x_st = Model.pack_x(trim.p_e, q_e, trim.v_e, w_b);
+x_dot = model.dynamics(x_st, [u_st; va_b]);
+[~, ~, v_e_dot, w_b_dot] = Model.unpack_x(x_dot);
 
-% Solve equations
-F_c = trim.get_F_c(model.m);
-L_b = model.I_b * w_b;
+% Numerical solver
 eqs = [
-    F_b == R_eb * [0; F_c; 0];  % Net forces
-    M_b == cross(w_b, L_b);     % Net moments
-    w_b == R_eb * trim.w_e;     % Body angle rate
-    R_eb(2, 1) == 0;            % Zero yaw
-    norm(q_e) == 1;             % Unit quaternion
+    v_e_dot == trim.V^2 / trim.R * [0; 1; 0];
+    w_b_dot == 0;
+    w_b == R_eb * trim.w_e;
+    R_eb(2, 1) == 0
+    norm(q_e) == 1
 ];
 q_e0 = [1; 0; 0; 0];
 w_b0 = trim.w_e;
@@ -43,7 +42,7 @@ sol = vpasolve(eqs, [q_e; w_b; u_st], [q_e0; w_b0; u0]);
 % Numerical evaluations
 q_e = double([sol.q_e1; sol.q_e2; sol.q_e3; sol.q_e4]);
 w_b = double([sol.w_b1; sol.w_b2; sol.w_b3]);
-x_st = pack_x(trim.p_e, q_e, trim.v_e, w_b);
+x_st = Model.pack_x(trim.p_e, q_e, trim.v_e, w_b);
 u_st = double([sol.u1; sol.u2; sol.u3; sol.u4]);
 
 end
