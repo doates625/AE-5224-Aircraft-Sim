032b42b43f9628f23d321a74a90a923d8d5feb34
Dan <doates625@gmail.com>
2020-02-20 14:19:11 -0500

    Added printouts of trim states

--- +AE5224/+quad_rotor/+trim/test.m
+++ +AE5224/+quad_rotor/+trim/test.m
@@ -16,7 +16,9 @@ import('AE5224.quad_rotor.Model');
 import('AE5224.quad_rotor.trim.solve');
 import('AE5224.quad_rotor.Log');
 import('AE5224.control.OpenLoop');
+import('AE5224.rigid_body.Model.unpack_x');
 import('AE5224.simulate');
+import('quat.Quat');
 
 % Default args
 if nargin < 1, t_max = 10.0; end
@@ -26,9 +28,27 @@ if nargin < 2, del_t = 0.01; end
 fprintf('Quadrotor Trim Test\n\n');
 
 % Trim solver
-fprintf('Solving for trim state...\n');
+fprintf('Solving for trim state...\n\n');
 model = Model();
 [x_st, u_st] = solve(model, trim);
+
+% Trim states
+[~, q_e, v_e, w_b] = unpack_x(x_st);
+q_e = Quat(q_e);
+[~, ty, tx] = q_e.euler();
+v_b = q_e.inv().rotate(v_e);
+fprintf('Trim States:\n');
+fprintf('- Pitch angle: %.2f [rad]\n', ty);
+fprintf('- Roll angle: %.2f [rad]\n', tx);
+fprintf('- Body velocity: [%+.2f, %+.2f, %+.2f] [rad]\n', v_b)
+fprintf('- Body angular: [%+.2f, %+.2f, %+.2f] [rad/s]\n\n', w_b);
+
+% Trim controls
+fprintf('Trim Controls:\n');
+fprintf('- Prop 1: %.0f [rpm]\n', u_st(1));
+fprintf('- Prop 2: %.0f [rpm]\n', u_st(2));
+fprintf('- Prop 3: %.0f [rpm]\n', u_st(3));
+fprintf('- Prop 4: %.0f [rpm]\n\n', u_st(4));
 
 % Simulate trim
 ctrl = OpenLoop(u_st, model.u_min, model.u_max);
