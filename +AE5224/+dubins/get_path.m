d7700aa7fbea2e376a3b5aca631bd1f87d01e12c
Dan <doates625@gmail.com>
2020-02-22 12:39:04 -0500

    Added function to get shortest dubins path

---
+++ +AE5224/+dubins/get_path.m
@@ -0,0 +1,39 @@
+function path = get_path(pa, ha, pb, hb, r)
+%path = GET_PATH(pa, ha, pb, hb, r)
+%   Get shortest dubins path from A to B
+%   
+%   Inputs:
+%   - pa = Point A [x; y]
+%   - ha = Heading A [rad]
+%   - pb = Point B [x; y]
+%   - hb = Heading B [rad]
+%   - r = Turn radius
+%   
+%   Outputs:
+%   - path = Path [AE5224.dubins.path.Path]
+
+% Imports
+import('AE5224.dubins.path.LSL');
+import('AE5224.dubins.path.LSR');
+import('AE5224.dubins.path.RSL');
+import('AE5224.dubins.path.RSR');
+
+% Find path
+paths = {@LSL, @LSR, @RSL, @RSR};
+best_path = [];
+best_dist = inf;
+for i = 1:4
+    % Try to make path
+    try path = paths{i}(pa, ha, pb, hb, r);
+    catch; continue; end
+    
+    % Compare to best
+    dist = path.dist();
+    if dist < best_dist
+        best_path = path;
+        best_dist = dist;
+    end
+end
+path = best_path;
+
+end
--- +AE5224/+dubins/test.m
+++ +AE5224/+dubins/test.m
@@ -6,6 +6,7 @@ import('AE5224.dubins.path.LSL');
 import('AE5224.dubins.path.LSR');
 import('AE5224.dubins.path.RSL');
 import('AE5224.dubins.path.RSR');
+import('AE5224.dubins.get_path');
 
 % Path conditions
 pa = 5*(2*rand(2, 1) - 1);
@@ -15,7 +16,7 @@ hb = pi*(2*rand()-1);
 r = 1;
 
 % Generate paths
-fprintf('Generating Dubins paths...\n');
+fprintf('Generating Dubins paths...\n\n');
 paths = {@LSL, @LSR, @RSL, @RSR};
 names = {'LSL', 'LSR', 'RSL', 'RSR'};
 for i = 1:4
@@ -26,8 +27,15 @@ for i = 1:4
         fig.Name = cls(end-2:end);
         path.plot(fig);
         status = 'Generated';
+        fprintf('Generated %s path (dist = %.1f).\n', names{i}, path.dist());
     catch
-        status = 'Failed';
+        fprintf('Failed %s path.\n', names{i});
     end
-    fprintf('%s %s path.\n', status, names{i});
+    
 end
+fprintf('\n');
+
+% Best path
+best_path = get_path(pa, ha, pb, hb, r);
+cls = class(best_path);
+fprintf('Best path: %s\n', cls(end-2:end))
